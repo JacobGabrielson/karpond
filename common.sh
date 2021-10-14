67acaed0e8d1e062ef126c8effb848fe06976d41
jacob <jacob@amazon.com>
2021-10-14 09:50:06 -0700

    fix shellcheck

--- common.sh
+++ common.sh
@@ -1,3 +1,4 @@
 export CLUSTER_NAME=$USER-karpenter-demo
 export AWS_DEFAULT_REGION=us-west-2
-export CLUSTER_ENDPOINT=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query "cluster.endpoint" --output json)
+CLUSTER_ENDPOINT=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query "cluster.endpoint" --output json)
+export CLUSTER_ENDPOINT

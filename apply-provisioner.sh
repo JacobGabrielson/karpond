d1443c8799d748c920f96bf9f5a80d219e922054
jacob <jacob@amazon.com>
2021-10-27 14:48:26 -0700

    update for v1alpha5

--- apply-provisioner.sh
+++ apply-provisioner.sh
@@ -3,22 +3,29 @@
 . ./common.sh
 
 cat <<EOF | kubectl apply -f -
-apiVersion: karpenter.sh/v1alpha4
+apiVersion: karpenter.sh/v1alpha5
 kind: Provisioner
 metadata:
   name: default
 spec:
+  requirements:
+    - key: "kubernetes.io/os" # If not included, all operating systems are considered
+      operator: In
+      values: ["linux"]
+    - key: "kubernetes.io/arch" # If not included, all architectures are considered
+      operator: In
+      values: ["amd64"]
   labels:
   #  node.k8s.aws/launch-template-name: "Karpenter-jacob-karpenter-demo-11759955701874416904"
      larry: sounders
      phil: swif
      pikov: andropov
   ttlSecondsAfterEmpty: 30
-  architectures: ['amd64']
+  #architectures: ['amd64']
   provider:
     #capacityTypes: ["spot", "on-demand"]
     #capacityTypes: ["on-demand"]
-    capacityTypes: ["spot"]
+    #capacityTypes: ["spot"]
     instanceProfile: KarpenterNodeInstanceProfile-${CLUSTER_NAME}
     cluster:
       name: ${CLUSTER_NAME}

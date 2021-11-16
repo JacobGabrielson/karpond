3573bd9eb677467cc4edbb189b81f8a0b366f3be
jacob <jacob@amazon.com>
2021-11-16 12:33:36 -0800

    updated after getting cluster working again

--- apply-provisioner.sh
+++ apply-provisioner.sh
@@ -2,29 +2,15 @@
 
 . ./common.sh
 
+# from karpenter/website/content/en/docs/getting-started/_index.md
+
 cat <<EOF | kubectl apply -f -
 apiVersion: karpenter.sh/v1alpha5
 kind: Provisioner
 metadata:
   name: default
 spec:
-  requirements:
-    - key: "kubernetes.io/os" # If not included, all operating systems are considered
-      operator: In
-      values: ["linux"]
-    - key: "kubernetes.io/arch" # If not included, all architectures are considered
-      operator: In
-      values: ["amd64"]
-  labels:
-  #  node.k8s.aws/launch-template-name: "Karpenter-jacob-karpenter-demo-11759955701874416904"
-     larry: sounders
-     phil: swif
-     pikov: andropov
-  ttlSecondsAfterEmpty: 30
-  #architectures: ['amd64']
   provider:
     instanceProfile: KarpenterNodeInstanceProfile-${CLUSTER_NAME}
-    cluster:
-      name: ${CLUSTER_NAME}
-      endpoint: $(aws eks describe-cluster --name ${CLUSTER_NAME} --query "cluster.endpoint" --output json)
+  ttlSecondsAfterEmpty: 30
 EOF
--- tag-subnets.sh
+++ tag-subnets.sh
@@ -6,6 +6,6 @@ SUBNET_IDS=$(aws cloudformation describe-stacks \
     --stack-name eksctl-${CLUSTER_NAME}-cluster \
     --query 'Stacks[].Outputs[?OutputKey==`SubnetsPrivate`].OutputValue' \
     --output text)
-aws ec2 create-tags \
+echo aws ec2 create-tags \
     --resources $(echo $SUBNET_IDS | tr ',' '\n') \
     --tags Key="kubernetes.io/cluster/${CLUSTER_NAME}",Value=
